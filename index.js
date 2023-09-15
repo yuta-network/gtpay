b053834a3a6984b4d80a067881b3fdd3ff7a1c31
Trần Công Tấn <88374323+yuta-network@users.noreply.github.com>
2023-09-15 18:06:07 +0700

    Splunk log

--- index.js
+++ index.js
@@ -64,6 +64,29 @@ app.post('/', function (req, res) {
 
 });
 
+var SplunkLogger = require("splunk-logging").Logger;
+
+var config = {
+    token: "your-token-here",
+    url: "https://splunk.local:8088"
+};
+
+var Logger = new SplunkLogger(config);
+
+var payload = {
+    // Message can be anything; doesn't have to be an object
+    message: {
+        temperature: "70F",
+        chickenCount: 500
+    }
+};
+
+console.log("Sending payload", payload);
+Logger.send(payload, function(err, resp, body) {
+    // If successful, body will be { text: 'Success', code: 0 }
+    console.log("Response from Splunk", body);
+});
+
 app.listen('80', () => {
     console.log("server started on port 3000")
 })
